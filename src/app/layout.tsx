874e3717ba3956d447e8db5bf9413821ec6e5390
Yashwant Kushwaha <ykkmec@gmail.com>
2025-04-17 12:26:52 +0530

    Added Generate Program Page Partially

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,10 +35,10 @@ export default function RootLayout({
           <Navbar />
 
           {/* GRID BACKGROUND */}
-          <div className="fixed inset-0 -z-1">
-            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
-            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
-          </div>
+          <div
+            className="absolute inset-0 z-0 bg-cover bg-center blur-md opacity-80"
+            style={{ backgroundImage: "url('/hero-ai33.png')" }}
+          />
 
           <main className="pt-24 flex-grow">{children}</main>
           <Footer />
